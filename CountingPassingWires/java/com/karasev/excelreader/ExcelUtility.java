d2af395970dbca234f70921126c51f27c949002c
nobodyRoman <progressivenation12@gmail.com>
2024-09-09 11:27:59 +0300

    update project for read Excel format .xlsx and .xls

--- CountingPassingWires/java/com/karasev/excelreader/ExcelUtility.java
+++ CountingPassingWires/java/com/karasev/excelreader/ExcelUtility.java
@@ -1,10 +1,8 @@
 package com.karasev.excelreader;
 
-import com.karasev.excelreader.entity.Wire;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -14,7 +12,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -34,49 +31,35 @@ public class ExcelUtility {
 
         String numberGroup = scannerSystem.nextLine();
 
-        List<Wire> wireList = excelToWireTable(file);
         List<String> groupNumber = new ArrayList<>();
 
         if (numberGroup.isEmpty()) {
             System.out.println("Адреса групп не введены! ");
             System.exit(0);
         } else {
-            groupNumber = List.of(numberGroup.split(" "));
+            groupNumber = List.of(numberGroup.toUpperCase().split(" "));
         }
 
-        int counterWires = 0;
-        char groupLetter = groupNumber.get(0).charAt(0);
-
-        for (Wire w : wireList) {
-            if (!(w.getFrom().isEmpty() || w.getTo().isEmpty())) {
-                if (groupNumber.contains(w.getFrom()) && (!groupNumber.contains(w.getTo()) && w.getTo().charAt(0) == groupLetter)) {
-                    counterWires++;
-                }
-                if (groupNumber.contains(w.getTo()) && (!groupNumber.contains(w.getFrom()) && w.getFrom().charAt(0) == groupLetter)) {
-                    counterWires++;
-                }
-                if ((!groupNumber.contains(w.getFrom()) && w.getFrom().charAt(0) == groupLetter) && w.getTo().charAt(0) != groupLetter) {
-                    counterWires++;
-                }
-                if (w.getFrom().charAt(0) != groupLetter && (!groupNumber.contains(w.getTo()) && w.getTo().charAt(0) == groupLetter)) {
-                    counterWires++;
-                }
-            }
-        }
+        int[] counterWires = readFileAndAnalysis(file, groupNumber);
 
         scannerSystem.close();
 
-        System.out.println("Количество проводов в жгуте: " + wireList.size() + " шт.");
-        System.out.println("Количество проводов проходящих через уплотнитель КР: " + counterWires + " шт.");
+        System.out.println("Количество проводов в жгуте: " + counterWires[1] + " шт.");
+        System.out.println("Количество проводов проходящих через уплотнитель КР: " + counterWires[0] + " шт.");
     }
 
+    //метод для проверки формата файла по расширению
     public static boolean hasExcelFormat(File file) {
         return file.getName().endsWith(XLSX) || file.getName().endsWith(XLS);
     }
 
-    public static List<Wire> excelToWireTable(File file) {
-        List<Wire> wireList = new ArrayList<>();
+    //Tметод для аналази полученого файла
+    private static int[] readFileAndAnalysis(File file, List<String> groupNumber) {
         String extension = FilenameUtils.getExtension(file.getName());
+        char groupLetter = groupNumber.get(0).charAt(0);
+        var cellFrom = 1;
+        var cellTo = 2;
+        int[] counterWires = new int[2];
 
         try {
             if (hasExcelFormat(file)) {
@@ -86,14 +69,14 @@ public class ExcelUtility {
                     XSSFWorkbook workbookXLSX = new XSSFWorkbook(fileInputStream);
                     XSSFSheet sheetXLSX = workbookXLSX.getSheetAt(0);
 
-                    readExcelSheetAndSaveWireEntityToList(sheetXLSX, wireList);
+                    counterWires = getCounterWires(sheetXLSX, groupNumber, groupLetter, cellFrom, cellTo);
 
                     workbookXLSX.close();
                 } else if (extension.equals(TYPE.XLS.getTitle())) {
                     HSSFWorkbook workbookXLS = new HSSFWorkbook(fileInputStream);
                     HSSFSheet sheetXLS = workbookXLS.getSheetAt(0);
 
-                    readExcelSheetAndSaveWireEntityToList(sheetXLS, wireList);
+                    counterWires = getCounterWires(sheetXLS, groupNumber, groupLetter, cellFrom, cellTo);
 
                     workbookXLS.close();
                 }
@@ -105,32 +88,37 @@ public class ExcelUtility {
             throw new RuntimeException("Fail to parse Excel file: " + e.getMessage());
         }
 
-        return wireList;
+        return counterWires;
     }
 
-    private static void readExcelSheetAndSaveWireEntityToList(Sheet sheet, List<Wire> wireList) {
+    //метод для подсчта количества проводов в листе и проводов проходящих через чехол КР
+    private static int[] getCounterWires(Sheet sheet,
+                                         List<String> groupNumber,
+                                         char groupLetter,
+                                         int cellFrom, int cellTo) {
+        int[] counterArrays = new int[2];
+
         for (Row currentRow : sheet) {
-            Iterator<Cell> cellsInRow = currentRow.iterator();
-            Wire wire = new Wire();
+            var from = currentRow.getCell(cellFrom).getStringCellValue().trim().toUpperCase();
+            var to = currentRow.getCell(cellTo).getStringCellValue().trim().toUpperCase();
 
-            int cellIndex = 0;
-
-            while (cellsInRow.hasNext()) {
-                Cell currentCell = cellsInRow.next();
-
-                switch (cellIndex) {
-                    case 0 -> wire.setId((int) currentCell.getNumericCellValue());
-                    case 1 -> wire.setFrom(currentCell.getStringCellValue().trim());
-                    case 2 -> wire.setTo(currentCell.getStringCellValue().trim());
-                    case 3 -> wire.setColor(currentCell.getStringCellValue().trim());
-                    case 4 -> wire.setCrossSection(currentCell.getNumericCellValue());
-                    case 5 -> wire.setLength((int) currentCell.getNumericCellValue());
-                    default -> {
-                    }
+            if (!(from.isEmpty() || to.isEmpty())) {
+                if (groupNumber.contains(from) && (!groupNumber.contains(to) && to.charAt(0) == groupLetter)) {
+                    counterArrays[0]++;
                 }
-                cellIndex++;
+                if (groupNumber.contains(to) && (!groupNumber.contains(from) && from.charAt(0) == groupLetter)) {
+                    counterArrays[0]++;
+                }
+                if ((!groupNumber.contains(from) && from.charAt(0) == groupLetter) && to.charAt(0) != groupLetter) {
+                    counterArrays[0]++;
+                }
+                if (from.charAt(0) != groupLetter && (!groupNumber.contains(to) && to.charAt(0) == groupLetter)) {
+                    counterArrays[0]++;
+                }
             }
-            wireList.add(wire);
+            counterArrays[1]++;
         }
+
+        return counterArrays;
     }
 }
